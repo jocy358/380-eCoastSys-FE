409bb7b32b7c6e4479f9329ab158cdb524936832
moros04 <166436342+moros04@users.noreply.github.com>
2026-04-27 13:05:10 -0700

    Initial commit

---
+++ eCoastSys/eCoastSys/eCoastSysApp.swift
@@ -0,0 +1,17 @@
+//
+//  eCoastSysApp.swift
+//  eCoastSys
+//
+//  Created by Miguel O on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct eCoastSysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
